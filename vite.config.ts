d5baa03a14fdc6ab2b20055c3b7c4283fe4942cb
Pavel_Melnik <129269794+miller-pavel@users.noreply.github.com>
2025-03-28 18:48:05 +0200

    json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build', // Або інша директорія
+      }
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
